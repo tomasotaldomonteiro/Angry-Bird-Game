859b3b9f645e22c0f3306b637a764e5631f86684
Tomás Monteiro <152517065+tomasotaldomonteiro@users.noreply.github.com>
2026-04-21 15:01:03 +0200

    Main Menu + some changes

---
+++ Assets/Main Menu Script.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+         public void PlayGame()
+            {
+                SceneManager.LoadScene("Level1"); // Trigger fade-out and load scene
+            }
+         
+            public void QuitGame()
+            {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+            }
+    
+}
